c3f5fcbb64918158c40827c1a366a24b640b56d9
Sean Feil <sean@com.tophatandmonocle>
2013-11-02 18:35:34 -0600

    Initial commit YAWDS

---
+++ index.php
@@ -0,0 +1,263 @@
+<!doctype HTML>
+<html lang="en">
+<head>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Coda' rel='stylesheet' type='text/css'>
+	<link href="css/site.css" rel="stylesheet" type="text/css">
+	<link href="css/style5.css" rel="stylesheet" type="text/css">
+	<title>Sean Feil Web</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+  <body>
+    <div class="container">
+
+    	<nav id="bt-menu" class="bt-menu">
+		<a href="#" class="bt-menu-trigger"><span>Menu</span></a>
+		<ul>
+			<li><a href="javascript:void(0)" class="scrollableNavElement" id="nav-top" data-scrolltarget="top">About Us</a></li>
+			<li><a href="javascript:void(0)" class="scrollableNavElement" id="nav-why" data-scrolltarget="why">Why YAWDS?</a></li>
+			<li><a href="javascript:void(0)" class="scrollableNavElement" id="nav-services" data-scrolltarget="services">Services</a></li>
+			<li><a href="javascript:void(0)" class="scrollableNavElement" id="nav-pricing" data-scrolltarget="pricing">Pricing</a></li>
+			<li><a href="javascript:void(0)" class="scrollableNavElement" id="nav-support" data-scrolltarget="support">Support Model</a></li>
+			<li><a href="javascript:void(0)" class="scrollableNavElement" id="nav-getstarted" data-scrolltarget="getstarted">Get Started</a></li>
+		</ul>
+	</nav>
+
+	<!-- TOP -->
+	<div id="top" class="pagefull">
+		<div id="top-bg-holder" class="bg-holder">
+			<img class="background-image">
+		</div>
+     	<div class="page-header">
+	     	<h1>YAWDS</h1>
+	     	<p class="lead">YAWDS Provides Taglines. TAGLINE.</p>
+	    </div>
+
+    	<h3>Why YAWDS</h3>
+     	<p>YAWDS provides great things</p>
+     	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing one
+    		</div>
+    	</div>
+
+    	<div class="row-spacer"></div>
+
+    	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing twol
+    		</div>
+    	</div>
+    </div>
+    <!--/TOP -->
+
+
+    <!-- WHY -->
+	<div id="why" class="pagefull">
+		<div id="why-bg-holder" class="why">
+			<img class="background-image">
+		</div>
+     	<div class="page-header">
+	     	<h1>WHY YAWDS</h1>
+	     	<p class="lead">YAWDS Provides Taglines. TAGLINE.</p>
+	    </div>
+
+    	<h3>Why YAWDS</h3>
+     	<p>YAWDS provides great things</p>
+     	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing one
+    		</div>
+    	</div>
+
+    	<div class="row-spacer"></div>
+
+    	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing twol
+    		</div>
+    	</div>
+    </div>
+    <!--/WHY -->
+
+    <!-- SERVICES -->
+	<div id="services" class="pagefull">
+		<div id="services-bg-holder" class="services">
+			<img class="background-image">
+		</div>
+     	<div class="page-header">
+	     	<h1>SERVICES YAWDS</h1>
+	     	<p class="lead">YAWDS Provides Taglines. TAGLINE.</p>
+	    </div>
+
+    	<h3>Why YAWDS</h3>
+     	<p>YAWDS provides great things</p>
+     	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing one
+    		</div>
+    	</div>
+
+    	<div class="row-spacer"></div>
+
+    	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing twol
+    		</div>
+    	</div>
+    </div>
+    <!--/SERVICES -->
+
+    <!-- PRICING -->
+	<div id="pricing" class="pagefull">
+		<div id="why-bg-pricing" class="pricing">
+			<img class="background-image">
+		</div>
+     	<div class="page-header">
+	     	<h1>PRICING YAWDS</h1>
+	     	<p class="lead">YAWDS Provides Taglines. TAGLINE.</p>
+	    </div>
+
+    	<h3>Why YAWDS</h3>
+     	<p>YAWDS provides great things</p>
+     	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing one
+    		</div>
+    	</div>
+
+    	<div class="row-spacer"></div>
+
+    	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing twol
+    		</div>
+    	</div>
+    </div>
+    <!--/PRICING -->
+
+    <!-- SUPPORT -->
+	<div id="support" class="pagefull">
+		<div id="support-bg-holder" class="support">
+			<img class="background-image">
+		</div>
+     	<div class="page-header">
+	     	<h1>SUPPORT YAWDS</h1>
+	     	<p class="lead">YAWDS Provides Taglines. TAGLINE.</p>
+	    </div>
+
+    	<h3>Why YAWDS</h3>
+     	<p>YAWDS provides great things</p>
+     	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing one
+    		</div>
+    	</div>
+
+    	<div class="row-spacer"></div>
+
+    	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing twol
+    		</div>
+    	</div>
+    </div>
+    <!--/SUPPORT -->
+
+    <!-- GETSTARTED -->
+	<div id="getstarted" class="pagefull">
+		<div id="getstarted-bg-holder" class="getstarted">
+			<img class="background-image">
+		</div>
+     	<div class="page-header">
+	     	<h1>GETSTARTED YAWDS</h1>
+	     	<p class="lead">YAWDS Provides Taglines. TAGLINE.</p>
+	    </div>
+
+    	<h3>Why YAWDS</h3>
+     	<p>YAWDS provides great things</p>
+     	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing one
+    		</div>
+    	</div>
+
+    	<div class="row-spacer"></div>
+
+    	<div class="row">
+    		<div class="col-sm-3 left-col">
+    			<b>great</b>
+    		</div>
+
+    		<div class="col-sm-6">
+    			Great thing twol
+    		</div>
+    	</div>
+    </div>
+    <!--/GETSTARTED -->
+
+	
+
+    <!-- Scripts -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.color.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/classie.js"></script>
+	<script type="text/javascript" src="js/jquery.parallax.js"></script>
+	<script type="text/javascript" src="js/borderMenu.js"></script>
+
+	<script type="text/javascript" src="js/core.js"></script>
+	<!--/Scripts -->
+</body>
+</html>
---
+++ js/core.js
@@ -0,0 +1,118 @@
+var currentBg = null;
+var currentFullView = null;
+
+$(document).ready(function() {
+
+console.info("startup");
+    // Change the initial size of the superstructure divs to match the page height (+ overhead)
+    sizePageFullElements();
+
+    // Handle initial element phasing
+    phasePageFullElement();
+
+    // Create navigation hooks
+    setupNavigationHooks();
+
+    // Set up the localscroll hooks
+    //$('#nav').localScroll();
+   // $('#toplink').localScroll();
+
+    // If the user resizes the window, resize the superstructure
+    $(window).resize(function() {
+        sizePageFullElements();
+        phasePageFullElement();
+    });
+
+    $(window).scroll(function() {
+        phasePageFullElement();
+    });
+});
+
+
+
+function setupNavigationHooks()
+{
+	var speed = 1000;
+
+	$('.scrollableNavElement').click(function() {
+        var _this = $(this);
+        $('html, body').animate({
+            scrollTop: $('#' + _this.data('scrolltarget')).offset().top + 5
+
+        }, speed);
+	})
+
+}
+
+/** Sets the size of each superstructure div to match that of the page height (+ overhead) **/
+function sizePageFullElements()
+{
+	$('.pagefull').each(function(index) {
+		$(this).css('min-height', $(window).height() + 200);
+	});
+}
+
+function phasePageFullElement()
+{
+	$('.pagefull').each(function(index, value) {
+			if(checkIfInFullView(value))
+			{
+				// This element is in view
+				if(value.id != currentFullView)
+					phaseIn(value.id);
+			}
+			else
+			{
+				if(value.id == currentFullView)
+					phaseOut(value.id);
+			}
+		});
+}
+
+function phaseIn(element)
+{
+	console.log("phasing in " + element);
+	currentFullView = element;
+	setActiveNav(element);
+	$('#' + element + '-bg-holder').fadeIn(900);
+}
+
+function phaseOut(element)
+{
+	console.log("phasing out " + element);
+	currentFullView = null;
+	$('#' + element + '-bg-holder').fadeOut(500);
+}
+
+function setActiveNav(element)
+{
+	$('.selectedNavElement').removeClass('selectedNavElement');
+	$('#nav-' + element).addClass('selectedNavElement');
+
+}
+
+function checkIfInView(element)
+{
+    var topoffset = $(element).offset().top - $(window).scrollTop();
+    var bottomoffset = $(element).height() + topoffset;
+
+    //console.log("top: " + topoffset + ", bottom: " + bottomoffset + ", innerheight: " + window.innerHeight);
+
+    if(topoffset < window.innerHeight && bottomoffset > window.innerHeight){
+        return true;
+    }
+   return false;
+}
+
+function checkIfInFullView(element)
+{
+    var topoffset = $(element).offset().top - $(window).scrollTop();
+    var bottomoffset = $(element).height() + topoffset;
+
+    //console.log(element.id + ">> top: " + topoffset + ", bottom: " + bottomoffset + ", innerheight: " + window.innerHeight + ", isinfullview: " + (topoffset < window.innerHeight && bottomoffset > window.innerHeight));
+
+    if(topoffset < window.innerHeight && bottomoffset >= window.innerHeight && topoffset <= 0){
+        return true;
+    }
+   return false;
+}
